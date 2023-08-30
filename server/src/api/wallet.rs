3c4ba0d9f29cbd23aec199bc2d562ac581e04d74
Einherjar <realeinherjar@proton.me>
2023-08-30 19:35:01 -0300

    fix(bdk): sync wallet over Esplora to taproot BIP86

--- server/src/api/wallet.rs
+++ server/src/api/wallet.rs
@@ -13,16 +13,29 @@ use serde_json::to_string;
 
 /// Creates a wallet from a mnemonic, a network type, and an internal and external derivation paths.
 pub fn create_wallet(
-    seed: Mnemonic,
-    network: Network,
-    derivation_path_external: DerivationPath,
-    derivation_path_internal: DerivationPath,
+    mnemonic: &str,
+    network: &str,
+    derivation_path_external: &str,
+    derivation_path_internal: &str,
 ) -> Result<Wallet> {
     let secp = Secp256k1::new();
 
+    let mnemonic = Mnemonic::parse_in(Language::English, mnemonic)?;
+    let network = match network {
+        "mainnet" => Network::Bitcoin,
+        "testnet" => Network::Testnet,
+        "signet" => Network::Signet,
+        "regtest" => Network::Regtest,
+        &_ => Network::Testnet, // NOTE: a good default
+    };
+
+    // generate derivation paths
+    let external_path = DerivationPath::from_str(derivation_path_external).unwrap();
+    let internal_path = DerivationPath::from_str(derivation_path_internal).unwrap();
+
     // generate external and internal descriptor from mnemonic
     let (external_descriptor, _ext_keymap) =
-        match descriptor!(wpkh((seed.clone(), derivation_path_external.clone())))
+        match descriptor!(tr((mnemonic.clone(), external_path))) // NOTE: taproot is hardcoded tr
             .unwrap()
             .into_wallet_descriptor(&secp, network)
         {
@@ -30,7 +43,7 @@ pub fn create_wallet(
             Err(e) => panic!("Invalid external derivation path: {}", e),
         };
     let (internal_descriptor, _int_keymap) =
-        match descriptor!(wpkh((seed.clone(), derivation_path_internal.clone())))
+        match descriptor!(tr((mnemonic.clone(), internal_path))) // NOTE: taproot is hardcoded tr
             .unwrap()
             .into_wallet_descriptor(&secp, network)
         {
@@ -60,19 +73,59 @@ pub async fn sync_wallet(wallet: &mut Wallet, client: &AsyncClient) -> Result<bo
     Ok(wallet.commit()?)
 }
 
-#[server(MyFun, "/api", "GetJson", "utxo")] // GetJson is a GET and will be cached
-pub async fn get_utxos(seed: String, network: String) -> Result<String, ServerFnError>{
-    let seed = Mnemonic::parse_in(Language::English, seed)?;
-    let network = if network == "bitcoin" { Network::Bitcoin } else { Network::Testnet };
-    let base_url = if network == Network::Bitcoin { "https://mempool.space/api" } else { "https://mempool.space/testnet/api" };
+/// Returns a JSON string of the wallet's utxos.
+#[server(GetUtxo, "/api", "GetJson", "utxo")] // GetJson is a GET and will be cached
+pub async fn get_utxo(mnemonic: String, network: String) -> Result<String, ServerFnError>{
+    // Create the Esplora async client
+    let base_url = if network == "bitcoin" { "https://mempool.space/api" } else { "https://mempool.space/testnet/api" };
     let esplora_client = Builder::new(base_url).build_async()?;
-    let mut wallet = create_wallet(seed, network,
-            DerivationPath::from_str("m/86'/0'/0'/0").unwrap(),
-            DerivationPath::from_str("m/86'/0'/0'/1").unwrap()).unwrap();
+
+    // Create the wallet
+    // NOTE: hardcoded to BIP86
+    let mut wallet = create_wallet(mnemonic.as_str(), network.as_str(),
+            "m/86'/0'/0'/0",
+            "m/86'/0'/0'/1").unwrap();
+
+    // Sync Wallet
     let _ = sync_wallet(&mut wallet, &esplora_client).await;
-    let utxos = wallet.list_unspent().collect::<Vec<LocalUtxo>>();
-    let json = to_string(&utxos)?;
+
+    // Get UTXOs
+    let utxo = wallet.list_unspent().collect::<Vec<LocalUtxo>>();
+
+    // Serialize to JSON
+    let json = to_string(&utxo)?;
     Ok(json)
+}
+
+/// Returns a JSON string of the wallet's balance.
+#[server(GetBalance, "/api", "GetJson", "balance")] // GetJson is a GET and will be cached
+pub async fn get_balance(mnemonic: String, network: String) -> Result<String, ServerFnError> {
+    // Create the Esplora async client
+    let base_url = if network == "bitcoin" { "https://mempool.space/api" } else { "https://mempool.space/testnet/api" };
+    let esplora_client = Builder::new(base_url).build_async()?;
+
+    // Create the wallet
+    // NOTE: hardcoded to BIP86
+    let mut wallet = create_wallet(mnemonic.as_str(), network.as_str(),
+            "m/86'/1'/0'/0",
+            "m/86'/1'/0'/1").unwrap();
+
+    // Sync Wallet
+    let _ = sync_wallet(&mut wallet, &esplora_client).await;
+
+    // Get Balance
+    let balance = wallet.get_balance();
+
+    // Serialize to JSON
+    let json = to_string(&balance)?;
+    Ok(json)
+}
+
+/// Returns a JSON string of the wallet's address for a given address type and index.
+/// Address type can be "receive" or "change".
+#[server(GetAdress, "/api", "GetJson", "address")] // GetJson is a GET and will be cached
+pub async fn get_address(mnemonic: String, network: String, address_type: &str, index: usize) -> Result<String, ServerFnError> {
+    todo!()
 }
 
 #[cfg(test)]
@@ -86,28 +139,28 @@ mod tests {
         let mnemonic_12 = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon cactus";
         let mnemonic_24 = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon art";
         let wallet_mainnet_12 = create_wallet(
-            Mnemonic::parse_in(Language::English, mnemonic_12).unwrap(),
-            Network::Bitcoin,
-            DerivationPath::from_str("m/86'/0'/0'/0").unwrap(),
-            DerivationPath::from_str("m/86'/0'/0'/1").unwrap()
+            mnemonic_12,
+            "mainnet",
+            "m/86'/0'/0'/0",
+            "m/86'/0'/0'/1",
         ).unwrap();
         let wallet_mainnet_24 = create_wallet(
-            Mnemonic::parse_in(Language::English, mnemonic_24).unwrap(),
-            Network::Bitcoin,
-            DerivationPath::from_str("m/86'/0'/0'/0").unwrap(),
-            DerivationPath::from_str("m/86'/0'/0'/1").unwrap()
+            mnemonic_24,
+            "mainnet",
+            "m/86'/0'/0'/0",
+            "m/86'/0'/0'/1",
         ).unwrap();
         let wallet_testnet_12 = create_wallet(
-            Mnemonic::parse_in(Language::English, mnemonic_12).unwrap(),
-            Network::Testnet,
-            DerivationPath::from_str("m/86'/0'/0'/0").unwrap(),
-            DerivationPath::from_str("m/86'/0'/0'/1").unwrap()
+            mnemonic_12,
+            "testnet",
+            "m/86'/0'/0'/0",
+            "m/86'/0'/0'/1",
         ).unwrap();
         let wallet_testnet_24 = create_wallet(
-            Mnemonic::parse_in(Language::English, mnemonic_24).unwrap(),
-            Network::Testnet,
-            DerivationPath::from_str("m/86'/0'/0'/0").unwrap(),
-            DerivationPath::from_str("m/86'/0'/0'/1").unwrap()
+            mnemonic_24,
+            "testnet",
+            "m/86'/0'/0'/0",
+            "m/86'/0'/0'/1",
         ).unwrap();
 
         assert!(is_wallet(&wallet_mainnet_12));
@@ -118,9 +171,5 @@ mod tests {
         assert_eq!(wallet_mainnet_24.network(), Network::Bitcoin);
         assert_eq!(wallet_testnet_12.network(), Network::Testnet);
         assert_eq!(wallet_testnet_24.network(), Network::Testnet);
-    }
-    #[tokio::test]
-    async fn test_balance() {
-        assert_eq!(balance().await.unwrap(), 0);
     }
 }
